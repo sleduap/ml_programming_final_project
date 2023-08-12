b9891a009a8be799a109a5f0afc050f71abc15e9
sleduap <sleduap@gmail.com>
2023-08-12 13:35:12 -0400

    add: implementation of paper code

---
+++ paper_code/XGBoost.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+import xgboost as xgb
+
+def xgb_scheduler(data,y_hat):
+    close = data.pop('close')
+    data.insert(5, 'close', close)
+    train, test = prepare_data(data, n_test=len(y_hat), n_in=6, n_out=1)
+    testY, y_hat2 = walk_forward_validation(train, test)
+    return testY, y_hat2
+
+def xgboost_forecast(train, testX):
+    # transform list into array
+    train = np.asarray(train)
+    # print('train', train)
+    # split into input and output columns
+    trainX, trainy = train[:, :-1], train[:, -1]
+    # print('trainX', trainX, 'trainy', trainy)
+    # fit model
+    model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=20)
+    model.fit(trainX, trainy)
+    # make a one-step prediction
+    yhat = model.predict(np.asarray([testX]))
+    return yhat[0]
+def walk_forward_validation(train, test):
+    predictions = list()
+    train = train.values
+    history = [x for x in train]
+    # print('history', history)
+    for i in range(len(test)):
+        testX, testy = test.iloc[i, :-1], test.iloc[i, -1]
+        # print('i', i, testX, testy)
+        yhat = xgboost_forecast(history, testX)
+        predictions.append(yhat)
+        history.append(test.iloc[i, :])
+        print(i+1, '>expected=%.6f, predicted=%.6f' % (testy, yhat))
+    return test.iloc[:, -1],predictions
+
+data = pd.read_csv('./paper_code/601988.SH.csv')
+data.index = pd.to_datetime(data['trade_date'], format='%Y%m%d')
+data = data.loc[:, ['open', 'high', 'low', 'close', 'vol', 'amount']]
+# data = pd.DataFrame(data, dtype=np.float64)
+close = data.pop('close')
+data.insert(5, 'close', close)
+data1 = data.iloc[3501:, 5]
+residuals = pd.read_csv('./ARIMA_residuals1.csv')
+residuals.index = pd.to_datetime(residuals['trade_date'])
+residuals.pop('trade_date')
+merge_data = pd.merge(data, residuals, on='trade_date')
+merge_data = merge_data.drop(labels='2007-01-04', axis=0)
+time = pd.Series(data.index[3501:])
+
+Lt = pd.read_csv('./ARIMA.csv')
+Lt = Lt.drop('trade_date', axis=1)
+Lt = np.array(Lt)
+Lt = Lt.flatten().tolist()
+
+train, test = prepare_data(merge_data, n_test=180, n_in=6, n_out=1)
+
+y, yhat = walk_forward_validation(train, test)
+plt.figure(figsize=(10, 6))
+plt.plot(time, y, label='Residuals')
+plt.plot(time, yhat, label='Predicted Residuals')
+plt.title('ARIMA+XGBoost: Residuals Prediction')
+plt.xlabel('Time', fontsize=12, verticalalignment='top')
+plt.ylabel('Residuals', fontsize=14, horizontalalignment='center')
+plt.legend()
+plt.show()
+
+finalpredicted_stock_price = [i + j for i, j in zip(Lt, yhat)]
+#print('final', finalpredicted_stock_price)
+evaluation_metric(data1, finalpredicted_stock_price)
+plt.figure(figsize=(10, 6))
+plt.plot(time, data1, label='Stock Price')
+plt.plot(time, finalpredicted_stock_price, label='Predicted Stock Price')
+plt.title('ARIMA+XGBoost: Stock Price Prediction')
+plt.xlabel('Time', fontsize=12, verticalalignment='top')
+plt.ylabel('Close', fontsize=14, horizontalalignment='center')
+plt.legend()
+plt.show()
